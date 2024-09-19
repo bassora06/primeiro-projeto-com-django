70bdc6208adb303e70ba8b3f8522061010c3dc3d
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-09-19 15:44:43 -0300

    barra de pesquisa e validação de exclusão de tarefa

--- projeto-django/tasks/views.py
+++ projeto-django/tasks/views.py
@@ -7,13 +7,22 @@ from django.contrib import messages
 # Create your views here.
 
 def taskList(request):
-    tasks_list = Task.objects.all().order_by('-created_at')
 
-    paginator = Paginator(tasks_list, 5)
+    search = request.GET.get('search')
 
-    page = request.GET.get('page')
+    if search:
 
-    tasks = paginator.get_page(page)
+        tasks = Task.objects.filter(title__icontains=search)
+        
+    else:
+
+        tasks_list = Task.objects.all().order_by('-created_at')
+         
+        paginator = Paginator(tasks_list, 5)
+
+        page = request.GET.get('page')
+
+        tasks = paginator.get_page(page)
 
     return render(request, 'tasks/list.html', {'tasks' : tasks})
 
