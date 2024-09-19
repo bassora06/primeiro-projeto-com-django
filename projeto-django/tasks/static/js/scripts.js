448308c34cfcca741bc761e204a3567aea4e57dc
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-09-19 14:14:34 -0300

    mudança no nome do projeto

---
+++ projeto-django/tasks/static/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    console.log("funcionando...")
+
+    var deleteBtn = $('.delete-btn');
+
+    $(deleteBtn).on('click', function(e){
+
+        e.preventDefault();
+
+    });
+});
