a7a9daefc997b2d400fc4c35dbede12dfb10303d
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-09-20 00:55:57 -0300

    colocando o crispy no projeto

--- projeto-django/todo/settings.py
+++ projeto-django/todo/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tasks',
     'users',
-    'about'
+    'about',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
